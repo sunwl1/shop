fc588ccff8d93335f5cb920ffb736b15e5d0cd01
shlee <shlee@bythem.net>
2021-12-01 14:35:32 +0900

    api 링크 수정

--- react-shop-app/client/src/setupProxy.js
+++ react-shop-app/client/src/setupProxy.js
@@ -1,11 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.set('port', process.env.PORT || 80);
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://ec2-54-180-88-25.ap-northeast-2.compute.amazonaws.com:5000',
+            target: 'http://54.180.88.25:5000',
             changeOrigin: true,
         })
     );
--- react-shop-app/server/index.js
+++ react-shop-app/server/index.js
@@ -15,7 +15,7 @@ const cookieParser = require("cookie-parser");
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017/DB", { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect("mongodb://localhost:27017/DB", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
