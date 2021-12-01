6b1feff63d3dfc854a50bc7a8c5f075a50a9f6cc
shlee <shlee@bythem.net>
2021-12-01 10:50:41 +0900

    포트 수정

--- react-shop-app/server/index.js
+++ react-shop-app/server/index.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 22
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server Running at ${port}`)
