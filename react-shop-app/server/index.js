fe1bb853d07c3e316b21853510195b0d4cf65e4d
shlee <shlee@bythem.net>
2021-12-01 11:15:17 +0900

    db 이름 수정

--- react-shop-app/server/index.js
+++ react-shop-app/server/index.js
@@ -15,7 +15,7 @@ const cookieParser = require("cookie-parser");
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017/DB", { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect("mongodb://192.168.0.100:27017/DB", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
